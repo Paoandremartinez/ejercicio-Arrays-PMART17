3bbc76db65baa6c8a2aa55e5977101d1277e0cdc
paoandremartinez <paoandremartinez@gmail.com>
2024-10-18 17:08:05 -0500

    Creacion Ejercicios Arrays

---
+++ src/EjercicioUno.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+public class EjercicioUno {
+    public static void main(String[] args) {
+
+
+
+    String fruits="Apple";
+    String fruits1="Banana";
+    String fruits2="Cherry";
+
+    List<String> listFruist = new ArrayList<>();
+    //Agrega los siguientes elementos
+    listFruist.add(fruits);
+    listFruist.add(fruits1);
+    listFruist.add(fruits2);
+    //Imprime el tamaño de la lista y los elementos.
+        System.out.println(listFruist.size());
+
+        //Elimina "Banana" de la lista.
+
+        listFruist.remove(1);
+        System.out.println(listFruist);
+
+    //Cambia el valor de "Cherry" por "Orange".
+        listFruist.add(2,"Cherry");
+        listFruist.set(2,"Orange");
+
+
+    //Imprime la lista resultante.
+        System.out.println(listFruist);
+
+}
+}
